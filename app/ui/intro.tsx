fe3be8251fba24b23136afd4b50c6c0df4b08b00
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-03-30 15:51:51 -0300

    chore: update intro text

--- app/ui/intro.tsx
+++ app/ui/intro.tsx
@@ -58,10 +58,10 @@ export default function Intro() {
         <span className="font-bold">
           Hello, I'm Alexandre Debortoli de Souza.
         </span>{" "}
-        I'm a <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
+        I'm a <span className="font-bold">full-stack software engineer</span>{" "}
+        with <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="underline">Node.js (NestJS)</span>.
       </motion.h1>
 
       <motion.div
