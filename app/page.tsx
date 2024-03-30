7317a93c31dde946a48dfba641dd4f29af6e9405
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-03-30 11:19:11 -0300

    feat: section divider component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/app/ui/intro";
+import SectionDivider from "@/app/ui/section-divider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider />
     </main>
   );
 }
