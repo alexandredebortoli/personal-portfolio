2e95867986605d418a3faa41a1fcff9af695548f
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-03-30 11:13:08 -0300

    feat: intro component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Intro from "@/app/ui/intro";
+
 export default function Home() {
-  return <main className=""></main>;
+  return (
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+    </main>
+  );
 }
