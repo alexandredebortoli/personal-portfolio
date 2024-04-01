79465746d74eab62d405382e8f9e789e8d20e42c
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-04-01 15:39:26 -0300

    fix: intro profile photo layout

--- app/ui/intro.tsx
+++ app/ui/intro.tsx
@@ -31,12 +31,10 @@ export default function Intro() {
           >
             <Image
               src={profilePhoto}
-              alt="Ricardo portrait"
-              width="192"
-              height="192"
+              alt="Alexandre portrait"
               quality="95"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-28 w-28 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
             />
           </motion.div>
 
